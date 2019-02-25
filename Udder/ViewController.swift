dfe7b38a38ea1bb2db480c484646db0540211111
Vitaly Badion <vitalybadion@badik-imac.local>
2019-02-25 09:07:09 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Vitaly Badion on 2/25/19.
+//  Copyright © 2019 Vitaly Badion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
